2aeaf300886ea103cdda56f7fd57a6cc41f940c9
fhag <gerard.fischer@outlook.com>
2016-10-15 10:47:36 +0200

    automatischer Batch erstellt

---
+++ Get+XLS+with+Prices.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+# ### get daily xls from KGAST site - needs to be run every day
+
+import urllib.request as request
+import time
+
+getxls = 'http://www.kgast.ch/dynasite.cfm?cmd=cprodukt_produkt_excel_export&skipfurl=1'
+
+# get first link for today's xls
+with request.urlopen(getxls) as response:
+   getxlslink = response.read().decode()
+
+#create filename
+fname = 'KGASTxls/KGAST'+time.strftime('%Y-%m-%d--%H%M%S')+'.xls'
+
+# save today's xls as fname
+fname, headers = request.urlretrieve(getxlslink, filename=fname)
+# show result and filename
+print(headers, fname)
+print('Download terminated for '+fname)
+
---
+++ getKGASTxls.bat
@@ -0,0 +1,2 @@
+cd C:\users\gfi 64bit\documents\visual studio 2015\projects\kgastkurse
+python get+xls+with+prices.py
